914d32b5c7447bb1df168989ab103ac9557359a6
Ricardo Vieira <ricmvieira@gmail.com>
2019-02-21 12:51:49 +0000

    Validations/Associations/Populating DB Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+# TODO: Write a seed to insert 100 posts in the database
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts "Creating fake ingredients..."
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+
+ingredients['drinks'].each do |i|
+  Ingredient.create(
+    name: i['strIngredient1']
+  )
+end
+
+puts "Finished!"
