9e31b988942cf66f3fb3081ab999973b3b7db2da
Ricardo Vieira <ricmvieira@gmail.com>
2019-02-22 11:54:52 +0000

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ require 'json'
 
 # TODO: Write a seed to insert 100 posts in the database
 
-puts 'Cleaning database...'
+puts 'Cleaning Ingredients database...'
 Ingredient.destroy_all
 
-puts "Creating fake ingredients..."
+puts 'Creating fake ingredients...'
 
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients = JSON.parse(open(url).read)
 
 ingredients['drinks'].each do |i|
@@ -17,4 +17,31 @@ ingredients['drinks'].each do |i|
   )
 end
 
-puts "Finished!"
+puts 'Finished with Ingredients database!'
+
+puts '####################################'
+
+puts 'Cleaning Cocktails database...'
+Cocktail.destroy_all
+
+cocktails_list = [
+  {
+    name: 'Black Russian',
+  },
+  {
+    name: 'Margarita',
+  },
+  {
+    name: 'Blood Orange & Star Anise',
+  },
+  {
+    name: "Bergamot Mojito",
+  },
+  {
+    name: 'Long Island'
+  }
+]
+Cocktail.create!(cocktails_list)
+puts 'Finished with Cocktails database!'
+
+puts "ALL DONE!"
